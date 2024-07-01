65779117f8e1e623628c9d0bafcbef12276bd300
dawkaka <yousiph77@gmail.com>
2024-07-01 20:50:59 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="apitoolkit_fastapi",
-    version="0.2.4",
+    version="1.2.0",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
